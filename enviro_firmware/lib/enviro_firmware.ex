00cacbd971ce03e67f03157de6d0173019d34540
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2020-11-21 22:15:09 -0500

    Initial commit
    Result of running `mix nerves.new enviro_firmware`

---
+++ enviro_firmware/lib/enviro_firmware.ex
@@ -0,0 +1,18 @@
+defmodule EnviroFirmware do
+  @moduledoc """
+  Documentation for EnviroFirmware.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> EnviroFirmware.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ enviro_firmware/test/enviro_firmware_test.exs
@@ -0,0 +1,8 @@
+defmodule EnviroFirmwareTest do
+  use ExUnit.Case
+  doctest EnviroFirmware
+
+  test "greets the world" do
+    assert EnviroFirmware.hello() == :world
+  end
+end
