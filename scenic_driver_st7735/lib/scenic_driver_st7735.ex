8b4c8eb0e6cb23462eb32d6bc3fc2fcfa4ace86a
Josh Davis <18429247+jdav-dev@users.noreply.github.com>
2020-11-29 13:49:46 -0500

    Create scenic_driver_st7735
    Result of running `mix new scenic_driver_st7735`

---
+++ scenic_driver_st7735/lib/scenic_driver_st7735.ex
@@ -0,0 +1,18 @@
+defmodule ScenicDriverSt7735 do
+  @moduledoc """
+  Documentation for `ScenicDriverSt7735`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScenicDriverSt7735.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ scenic_driver_st7735/test/scenic_driver_st7735_test.exs
@@ -0,0 +1,8 @@
+defmodule ScenicDriverSt7735Test do
+  use ExUnit.Case
+  doctest ScenicDriverSt7735
+
+  test "greets the world" do
+    assert ScenicDriverSt7735.hello() == :world
+  end
+end
